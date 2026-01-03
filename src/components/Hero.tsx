4afa2ddff81697aecb63786f3415cf5123422d12
SubhanHakim <subhanhakim07@gmail.com>
2026-01-03 11:36:51 +0700

    update x

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -71,7 +71,7 @@ const Hero = () => {
                     transition={{ delay: 2.2, duration: 1 }}
                     className="mt-12 flex gap-4 md:gap-6 z-30"
                 >
-                    <SocialButton href="#" icon={<img src={dexIcon} alt="Dex" className="w-4 h-4 opacity-70 group-hover:opacity-100 transition-opacity" />} label="pumpfun" />
+                    <SocialButton href="https://pump.fun/coin/g6ZrCZHdAVZiXeDX1hQAXAEhPnHoyeCFEVwgFsVpump" icon={<img src={dexIcon} alt="Dex" className="w-4 h-4 opacity-70 group-hover:opacity-100 transition-opacity" />} label="pumpfun" />
                     <SocialButton href="https://x.com/veil_line" icon={<XIcon />} label="X" />
                 </motion.div>
             </motion.div>
